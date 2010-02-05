71a98b64c4e4d3a2926b43e94c6055d3d4155d76
Vitaly Parfonov <vitaly.parfonov@gmail.com>
2010-02-05 17:00:57 +0000

    Remove Demo Gdagte build from from xcmis-server-tomcat module

--- xcmis-server-war/src/main/webapp/org.xcmis.demo.gadget.GadgetWrapper/applicationInitializer.js
+++
@@ -1,4 +0,0 @@
-var appConfig = {
-		"gadgetServer" : "/xcmis/gadgets/", /*for portal*/
-		"gadgeturl" : "http://gwt.demo.exoplatform.org/CmisClientRemote/org.exoplatform.cmis.CmisClientGadget/org.exoplatform.cmis.client.CmisClientGadget.gadget.xml"
-	}
