bd54f1e8c7a81b958642364626ef8f4ef1651025
Alexey Zavizionov <alexey.zavizionov@gmail.com>
2010-02-10 11:43:39 +0000

    Modified WADL links. Cleanup xcmis.sh and xcmis.bat.

--- xcmis-server-tomcat-demo/src/main/resources/bin/xcmis.bat
+++ xcmis-server-tomcat-demo/src/main/resources/bin/xcmis.bat
@@ -8,10 +8,7 @@ for %%i in ( !%BIN_DIR%\..! ) do set TOMCAT_HOME=%%~dpni
 rem Sets some variables
 set LOG_OPTS="-Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.Log4JLogger"
 set SECURITY_OPTS="-Djava.security.auth.login.config=%TOMCAT_HOME%\conf\jaas.conf"
-set EXO_OPTS="-Dexo.product.developing=true"
-set JAVA_OPTS=-Xshare:auto -Xms128m -Xmx512m %LOG_OPTS% %SECURITY_OPTS% %EXO_OPTS%
-set JPDA_TRANSPORT=dt_socket
-set JPDA_ADDRESS=8000
+set JAVA_OPTS=-Xshare:auto -Xms128m -Xmx512m %LOG_OPTS% %SECURITY_OPTS%
 
 rem Launches the server
 cd %BIN_DIR%
--- xcmis-server-tomcat-demo/src/main/resources/bin/xcmis.sh
+++ xcmis-server-tomcat-demo/src/main/resources/bin/xcmis.sh
@@ -6,22 +6,11 @@ cd `dirname "$0"`
 # Sets some variables
 LOG_OPTS="-Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.Log4JLogger"
 SECURITY_OPTS="-Djava.security.auth.login.config=../conf/jaas.conf"
-EXO_CONFIG_OPTS="-Xshare:auto -Xms128m -Xmx512m -Dorg.exoplatform.container.configuration.debug"
-
-JPDA_TRANSPORT=dt_socket
-JPDA_ADDRESS=8000
+EXO_CONFIG_OPTS="-Xshare:auto -Xms128m -Xmx512m"
 
 #REMOTE_DEBUG="-Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n"
 
-JMX_AGENT="-Dcom.sun.management.jmxremote"
-
-# For profiling
-#LD_LIBRARY_PATH="/cygdrive/d/tools/YourKit/bin/win32/"
-#PATH="$PATH:$LD_LIBRARY_PATH"
-#export LD_LIBRARY_PATH
-#YOURKIT_PROFILE_OPTION="-agentlib:yjpagent  -Djava.awt.headless=true"
-
-JAVA_OPTS="$YOURKIT_PROFILE_OPTION $JAVA_OPTS $LOG_OPTS $SECURITY_OPTS $EXO_OPTS $EXO_CONFIG_OPTS $REMOTE_DEBUG"
+JAVA_OPTS="$JAVA_OPTS $LOG_OPTS $SECURITY_OPTS $EXO_CONFIG_OPTS $REMOTE_DEBUG"
 export JAVA_OPTS
 
 # Launches the server
