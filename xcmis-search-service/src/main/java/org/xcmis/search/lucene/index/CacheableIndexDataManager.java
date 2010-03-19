d23c2a87d39eff69c1b7af069fcae92012370823
Sergey Kabashnyuk <ksmster@gmail.com>
2010-03-19 13:40:15 +0000

    CMIS-219 :  removed lucene force persistence workaround

--- xcmis-search-service/src/main/java/org/xcmis/search/lucene/index/CacheableIndexDataManager.java
+++ xcmis-search-service/src/main/java/org/xcmis/search/lucene/index/CacheableIndexDataManager.java
@@ -27,6 +27,7 @@ import org.exoplatform.services.log.Log;
 import org.xcmis.search.config.IndexConfigurationException;
 import org.xcmis.search.config.IndexConfuguration;
 import org.xcmis.search.lucene.index.merge.AggregatePolicy;
+import org.xcmis.search.lucene.index.merge.DocumentCountAggregatePolicy;
 import org.xcmis.search.lucene.index.merge.MaxCandidatsCountAggrigatePolicy;
 import org.xcmis.search.lucene.index.merge.PendingAggregatePolicy;
 
@@ -287,7 +288,7 @@ public class CacheableIndexDataManager extends LocalIndexDataManagerProxy
          }
          // if transaction have less then DEFAULT_MAX_DOCUMENTS_4_DIR to add then
          // create memory index
-         if (0 > changes.getAddedDocuments().size())
+         if (DocumentCountAggregatePolicy.DEFAULT_MAX_DOCUMENTS_4_DIR > changes.getAddedDocuments().size())
          {
             changes = changes.apply(processAdded(changes));
          }
