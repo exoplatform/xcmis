f250ac9fee08ae5ed018c1dce3175f64cec60d24
Gennady Azarenkov <gazarenkov@gmail.com>
2010-04-14 12:14:17 +0000

    CmisService abstraction along with deploy package with a scratches of couple impl

---
+++ xcmis-spi/src/main/java/org/xcmis/spi/deploy/SingletonCmisService.java
@@ -0,0 +1,46 @@
+package org.xcmis.spi.deploy;
+/*
+ * Copyright (C) 2010 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+import org.xcmis.spi.CmisService;
+import org.xcmis.spi.StorageProvider;
+/**
+ * @version $Id:$
+ */
+public class SingletonCmisService extends CmisService {
+  
+  static private CmisService instance = null; 
+  
+  public static CmisService setInstance(CmisService inst)
+  {
+    if(instance == null)
+     instance = inst;
+    return instance;
+  }
+  
+  public static CmisService getInstance()
+  {
+    return instance;
+  }
+  
+  public final void addStorage(String id, StorageProvider storageProvider) 
+  {
+    this.storageProviders.put(id, storageProvider);
+  }
+
+}
