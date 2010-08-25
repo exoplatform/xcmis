d521ee767adacc528817c989541111863805c2e2
Sergey.Kryzhny <Sergey.Kryzhny@gmail.com>
2010-08-25 15:09:17 +0000

    create branch dir for ADM-1610

--- xcmis-server-tomcat-demo/README.txt
+++
@@ -1,2 +0,0 @@
-To assemble on Tomcat use the command
-mvn clean install -Passembly -Dtomcat.distrib=/PATH/TO/TOMCAT/DISTRIBUTIVE
--- xcmis-server-tomcat-demo/src/main/webapp/xcmis-demo-gadget/applicationInitializer.js
+++
@@ -1,5 +0,0 @@
-var appConfig = {
-		"gadgetServer" : "/xcmis/gadgets/", /*for portal*/
-		"gadgeturl" : "http://xcmis.org/CmisExpertRemote/org.exoplatform.cmis.CmisExpertGadget/org.exoplatform.cmis.client.CmisExpertGadget.gadget.xml"
-	}
-//"gadgeturl" : "http://gwt.demo.exoplatform.org/CmisClientRemote/org.exoplatform.cmis.CmisClientGadget/org.exoplatform.cmis.client.CmisClientGadget.gadget.xml"
