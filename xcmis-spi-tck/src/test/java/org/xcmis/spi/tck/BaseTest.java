08f24a07f1486b0b162a7d69bfb6bb38a6f7d852
Andrey Parfonov <andrew00x@gmail.com>
2010-09-05 09:33:10 +0000

    CMIS-426

--- xcmis-spi-tck/src/test/java/org/xcmis/spi/tck/BaseTest.java
+++ xcmis-spi-tck/src/test/java/org/xcmis/spi/tck/BaseTest.java
@@ -176,6 +176,7 @@ public class BaseTest
             removeRelationships(root);
          }
          connection.deleteTree(root, true, UnfileObject.DELETE, true);
+         connection.close();
       }
       catch (Exception e)
       {
