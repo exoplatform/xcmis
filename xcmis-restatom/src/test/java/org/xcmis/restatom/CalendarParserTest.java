993d959dfc7547b80b720120926e4402fdba3f13
Alexey Zavizionov <alexey.zavizionov@gmail.com>
2011-05-13 11:25:05 +0000

    Modified the spell mistake CallendarParserTest -> CalendarParserTest

--- xcmis-restatom/src/test/java/org/xcmis/restatom/CalendarParserTest.java
+++ xcmis-restatom/src/test/java/org/xcmis/restatom/CalendarParserTest.java
@@ -27,7 +27,7 @@ import java.util.Calendar;
  * @author <a href="mailto:andrey.parfonov@exoplatform.com">Andrey Parfonov</a>
  * @version $Id: CallendarParserTest.java 2 2010-02-04 17:21:49Z andrew00x $
  */
-public class CallendarParserTest extends TestCase
+public class CalendarParserTest extends TestCase
 {
 
    public void testGetString()
